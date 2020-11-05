b74f9f44591e4078d2bf61dec17128ab6b0df930
Miguel  Arreola <marreola4619@gmail.com>
2020-11-05 20:33:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-  if string[0] == "s"binding.pry
-    
+  if string[0] == "s"
+    binding.pry
     10 * "s" + string
   else
     string
